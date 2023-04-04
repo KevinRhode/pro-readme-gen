18bf2caf440057200e7718b7f7943a757b128ada
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-04 19:05:43 -0400

    add: all questions added, starting on init function

--- index.js
+++ index.js
@@ -20,11 +20,64 @@ const questions = [
         pre: '',
         
     },
-   
-
-
-
-
+    {
+        type: 'input',
+        name: 'name',
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        title: "installation",
+        pre: '',
+      },
+      {
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        name: 'name', 
+        title:'usage'     
+      },
+      {
+        type:'input',
+        message:'Enter description',
+        name: 'name',
+        prefix: `Enter info for contributer links, description first then link\n`,
+        title:'contributingDesc'
+      },
+      {
+        type:'input',
+        message:'Enter Link',
+        name: 'name',
+        title:'contributingSrc'
+      },
+      {
+        type: 'list',
+        name: 'name',
+        message: "Continue with another Contributer?",
+        choices: ["No","Yes"],
+        title: "continueContr",
+      },
+      {
+        type:'input',
+        message:'Enter Guidelines for testing',
+        name: 'name',
+        title:'tests'
+      },
+      {
+        type: 'list',
+        message: 'What license will this project be covered by?',
+        name: 'name',
+        choices: ['GNU GPLv3', 'Apache License 2.0', 'MIT License'],
+        title:'license'
+      },
+      {
+        type:'input',
+        message:'Enter GitHub username:',
+        name: 'name',
+        title:'questions'
+      },
+      {
+        type:'input',
+        message:'Enter email to be reached at:',
+        name: 'name',
+        title:'questions'
+      },
 ];
 
 // TODO: Create a function to write README file

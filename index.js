f83ac3cd7c1ee2184a826381b267907dd622ef51
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-05 22:45:47 -0400

    add: class for questions of prompts

--- index.js
+++ index.js
@@ -5,35 +5,92 @@ const fs = require('fs/promises');
 const generateMarkDown = require('./utils/generateMarkdown.js');
 // include the inquirer module
 const inquirer = require('inquirer');
-
+// TODO: Create Class for question
+class question {
+  constructor(type,name,message,choices)  {
+    this.type = type;
+    this.name = name;
+    this.message = message;
+    this.choices = choices?.value;    
+    //choices === undefined ? '' : choices;
+  }
+}
 // TODO: Create an array of questions for user input
 const questions = [
 
-    {
-        type: 'input',
-        name: 'title',
-        message: "What is Title of this README?",       
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: "What is the description of this README?",        
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-    },
-    {
-      type: 'input',
-      message: 'Provide instructions and examples for use. Include screenshots as needed.',
-      name: 'usage',     
-    },
-    {
-        type:'input',
-        message:'Enter Contributing guidelines for users',
-        name: `contributing` 
-    },
+  new question(
+    'input',
+    'title',
+    'What is Title of this README?'
+  ),
+  new question(
+    'input',
+    'description',
+    'What is the description of this README?'
+  ),
+  new question(
+    'input',
+    'installation',
+    'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+  ),
+  new question(
+    'input',
+    'title',
+    'Provide instructions and examples for use. Include screenshots as needed.'
+  ),
+  new question(
+    'input',
+    'contributing',
+    'Enter Contributing guidelines for users'
+  ),
+  new question(
+    'input',
+    'tests',
+    'Enter Guidelines for testing'
+  ),
+  new question(
+    'list',
+    'license',
+    'What license will this project be covered by?',
+    ['GNU GPLv3', 'Apache License 2.0', 'MIT License']
+  ),
+  new question(
+    'input',
+    'questions.userName',
+    'Enter GitHub username:'
+  ),
+  new question(
+    'input',
+    'questions.email',
+    'Enter email to be reached at:',
+  )
+  
+    // {
+
+    //     type: 'input',
+    //     name: 'title',
+    //     message: "What is Title of this README?",       
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'description',
+    //     message: "What is the description of this README?",        
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'installation',
+    //   message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+    // },
+    // {
+    //   type: 'input',
+    //   message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    //   name: 'usage',     
+    // },
+    // {
+    //     type:'input',
+    //     message:'Enter Contributing guidelines for users',
+    //     name: `contributing` 
+    // },
     // {
     //   type:'input',
     //   message:'Enter description',
@@ -46,29 +103,28 @@ const questions = [
     //   message:'Enter Link',
     //   name: `contributing.src`      
     // },  
-    {
-      type:'input',
-      message:'Enter Guidelines for testing',
-      name: 'tests',
-    },
-    {
-      type: 'list',
-      message: 'What license will this project be covered by?',
-      name: 'license',
-      choices: ['GNU GPLv3', 'Apache License 2.0', 'MIT License'],
-    },
-    {
-      type:'input',
-      message:'Enter GitHub username:',
-      name: 'questions.userName',
-    },
-    {
-      type:'input',
-      message:'Enter email to be reached at:',
-      name: 'questions.email',
-    }
+    // {
+    //   type:'input',
+    //   message:'Enter Guidelines for testing',
+    //   name: 'tests',
+    // },
+    // {
+    //   type: 'list',
+    //   message: 'What license will this project be covered by?',
+    //   name: 'license',
+    //   choices: ['GNU GPLv3', 'Apache License 2.0', 'MIT License'],
+    // },
+    // {
+    //   type:'input',
+    //   message:'Enter GitHub username:',
+    //   name: 'questions.userName',
+    // },
+    // {
+    //   type:'input',
+    //   message:'Enter email to be reached at:',
+    //   name: 'questions.email',
+    // }
 ];
-
 // TODO: Create a function to write README file
 async function writeToFile(fileName, data) {
 //create log file name based of location
@@ -77,15 +133,12 @@ const logFilename = process.argv[1].substring(0,process.argv[1].lastIndexOf("\\"
 fs.appendFile(logFilename,JSON.stringify(data));
 //write file to create a new generated readme based on the information input
 fs.writeFile(fileName,generateMarkDown(data))
-
 }
-
 // TODO: Create a function to initialize app
 async function init() {
   //set filename for the generated readme
   const filename = process.argv[1].substring(0,process.argv[1].lastIndexOf("\\") + 1) + 'generatedREADME.md';
-  // const filenamejson = process.argv[1].substring(0,process.argv[1].lastIndexOf("\\") + 1) + 'readMeData.json';
-  
+  // const filenamejson = process.argv[1].substring(0,process.argv[1].lastIndexOf("\\") + 1) + 'readMeData.json';  
   // check if someone added arguments when calling index.js
   if (process.argv.length > 2) {
     //test on key, see what they added
@@ -103,19 +156,14 @@ async function init() {
     }
 
   } else {
-
     //a sync function to await all questions before response, then async to make sure await, along with fs import (fs/promises)
     await inquirer.prompt(questions).then(async (response) =>{
         //log the response for testing
         // console.log(response);
         //call write to file function to handle fs implementation
         writeToFile(filename,response);        
-    });   
-    
+    });       
   }
-  
-
 }
-
 // Function call to initialize app
 init();
